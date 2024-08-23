db948ea3b00a136c6b410574e666291ac1abc24c
mafeermoreno <marifermg29@gmail.com>
2024-08-22 21:03:47 -0600

    cross-entropy error

--- main.py
+++ main.py
@@ -36,19 +36,16 @@ print(f"Number of 0's in the 'Snowfall' column: {zero_snowfall.sum()}/{len(df)}"
 print() 
 
 # Create a copy of the dataset in csv format
-df.to_csv('SeoulBikeData_clean.csv', index=False)
+df.to_csv('SeoulBikeData_clean.csv', index = False)
 
 # Read the new dataset
-df_clean = pd.read_csv('SeoulBikeData_clean.csv', encoding='ISO-8859-1')
+df_clean = pd.read_csv('SeoulBikeData_clean.csv', encoding ='ISO-8859-1')
 
 # Delete from the copy the features 'Solar radiation(MJ/m2)', 'Rainfall(mm)' and 'Snowfall (cm)' (Fully missing II)
-df_clean.drop(['Solar Radiation (MJ/m2)','Rainfall(mm)', 'Snowfall (cm)'], axis=1, inplace=True)
-print(df_clean.head())
-print()
-df_clean.info()
+df_clean.drop(['Solar Radiation (MJ/m2)','Rainfall(mm)', 'Snowfall (cm)'], axis=1, inplace = True)
 
 # Apply one-hot encoding to the 'Seasons' and 'Holiday' features
-df_encoded = pd.get_dummies(df_clean, columns=['Seasons', 'Holiday'], prefix='', prefix_sep='')
+df_encoded = pd.get_dummies(df_clean, columns=['Seasons', 'Holiday'], prefix ='', prefix_sep ='')
 df_encoded['Spring'] = df_encoded['Spring'].astype(int)
 df_encoded['Summer'] = df_encoded['Summer'].astype(int)
 df_encoded['Autumn'] = df_encoded['Autumn'].astype(int)
@@ -56,25 +53,30 @@ df_encoded['Winter'] = df_encoded['Winter'].astype(int)
 df_encoded['Holiday'] = df_encoded['Holiday'].astype(int)
 df_encoded['No Holiday'] = df_encoded['No Holiday'].astype(int)
 df_encoded.columns = df_encoded.columns.str.strip()
-print(df_encoded.columns)
-'''
-print(df_encoded.head())
-print(df_encoded.tail())
-print(df_encoded.info())
-'''
 
 # Scalarize the dataset
 scaled_features = ['Rented Bike Count', 'Hour', 'Temperature(Â°C)', 'Humidity(%)', 'Wind speed (m/s)', 'Visibility (10m)', 'Dew point temperature(Â°C)']
 df_encoded[scaled_features] = df_encoded[scaled_features] - df_encoded[scaled_features].mean() / df_encoded[scaled_features].std()
-print(df_encoded.head())
 
 # Hyphotesis function
 def h(params, sample, bias):
     acum = bias
-    print('acum desde 0 ',acum)
     for i in range(len(params)):
         acum += params[i] * sample[i]
     
     # Apply the sigmoid function to the linear combination
     return 1 / (1 + np.exp(-acum))
 
+# Percentage of error
+def error_perc (params, sample, y, bias):
+    N = len(y) 
+    total_error = 0
+    
+    for i in range(N):
+        # Predict the probability using the hypothesis function
+        prediction = h(params, sample[i], bias)
+        # Calculate the cross-entropy for the sample
+        total_error += -y[i] * np.log(prediction) - (1 - y[i]) * np.log(1 - prediction)
+    
+    # Average error
+    return total_error / N
